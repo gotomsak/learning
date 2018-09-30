612cd27f6588be91a04bfd2e0948e5862fdcd73d
goto <gotomsak@gmail.com>
2018-09-30 14:16:55 +0900

    first commit

---
+++ mnist_test.py
@@ -0,0 +1,40 @@
+from keras.datasets import mnist
+from keras.utils.np_utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# 画像を1次元化
+x_train = x_train.reshape(60000, 784)
+x_test = x_test.reshape(10000, 784)
+
+
+# 画素を0~1の範囲に変換(正規化)
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+
+# 正解ラベルをone-hot-encoding
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+model = Sequential()
+model.add(Dense(64, activation='relu', input_dim=784))
+model.add(Dense(10, activation='softmax'))
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+            batch_size=100,
+            epochs=12,
+            verbose=1)
+
+score = model.evaluate(x_test, y_test)
+# print(score[0])
+# print(score[1])
+
+# print(len(x_train))
+# print(len(x_test))
+# print(len(y_train))
+# print(len(y_test))
